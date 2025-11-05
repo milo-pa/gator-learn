aa35f4a3f4d3eb2a90abe5bbb8085b61805d4434
Milo <milopesce@gmail.com>
2025-11-04 21:57:52 -0800

    Tracking pages and team member clicks

--- application/frontend/src/component/TeamMembersList.js
+++ application/frontend/src/component/TeamMembersList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import TeamMembersService from "../service/TeamMembersService.js";
 import TeamMemberComponent from "./TeamMemberComponent.js";
 import { Link } from "react-router-dom";
+import { trackEvent } from "../analytics/ga";
 
 class TeamMembersList extends Component {
     constructor(props) {
@@ -22,6 +23,13 @@ class TeamMembersList extends Component {
             });
     }
 
+    handleMemberClick(member) {
+        trackEvent("team_member_clicked", {
+            memberName: member.name,
+            memberTitle: member.title,
+        });
+    }
+
     render() {
         if (this.state.error) {
             return <div>{this.state.error}</div>;
@@ -33,13 +41,14 @@ class TeamMembersList extends Component {
                     {this.state.members.map((member) => (
                         <Link key={member.name}
                               to={`/team-members/${encodeURIComponent( member.name)}`}
-                              className="tile-link">
+                              className="tile-link"
+                              onClick={() => this.handleMemberClick(member)}
+                        >
                             <TeamMemberComponent
                                 name={member.name}
                                 title={member.title}
                                 imagePath={member.imagePath} />
                         </Link>
-
                     ))}
                 </div>
             </div>
