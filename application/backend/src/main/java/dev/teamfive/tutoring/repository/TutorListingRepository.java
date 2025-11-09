891e27885d0a45858152ad23404e706375d87e45
j-hammond1 <jhammond.123x@gmail.com>
2025-11-08 23:52:21 -0800

    Add headers to two main .java files

--- application/backend/src/main/java/dev/teamfive/tutoring/repository/TutorListingRepository.java
+++ application/backend/src/main/java/dev/teamfive/tutoring/repository/TutorListingRepository.java
@@ -1,3 +1,16 @@
+/*
+ * Institution: San Francisco State University
+ * Class: CSC 648 Project, Team 05
+ * Project: Gator Learn, Tutoring Website
+ * Author: Jonah H
+ * Created: November 8th 2025
+ * Description: An interface defining ways to interact with TutorListing entities from the `tutor_listing` database table.
+ *
+ * Copyright (c) 2025 San Francisco State University Team 05
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 package dev.teamfive.tutoring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
