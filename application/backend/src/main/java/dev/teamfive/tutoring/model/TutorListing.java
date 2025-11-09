2ecc565d1fea2314b0158f1208f1cebcbf6d5fa0
j-hammond1 <jhammond.123x@gmail.com>
2025-11-08 18:46:28 -0800

    Fix TutorListing database entity Model

--- application/backend/src/main/java/dev/teamfive/tutoring/model/TutorListing.java
+++ application/backend/src/main/java/dev/teamfive/tutoring/model/TutorListing.java
@@ -1,9 +1,13 @@
 package dev.teamfive.tutoring.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 // Each instance of a TutorListing represents a row in the db table 'tutor_listing'
@@ -11,49 +15,145 @@ import jakarta.persistence.Table;
 @Table(name = "tutor_listing")
 public class TutorListing
 {
-    // Automatically populate primary key from table row
     @Id
+    // Tells JPA how to generate the listing_id if creating a new listing (GenerationType.IDENTITY means use db's auto-increment column)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "listing_id")
     private Long listingId;
 
+    @Column(name = "user_id")
     private Long userId;
 
-    // TODO: in m2 doc these two are just listed as 'subject' and 'course', implying they are names - however they are
-    //  noted as foreign keys to subject and course tables, which should have subject and course ids as primary keys -
-    //  so for these to be foreign keys to those tables they must actually be ids rather than subject/course names?
-    private Long subjectId;
-    private Long courseId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subject_id")
+    private Subject subject;
 
-    // TODO: how are prices stored in db?
-    private double price;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
 
-    // TODO: this turns out to be a bit complicated
-    //  I assume this is available times per week?
-    //  - Could be a list of pairs of dates within a week, where 1st member of the pair is starting time and 2nd member
-    //    is ending time (e.g. mon 2:30-4, weds 4-5)
-    //    - So each availble time in a week could be millis since epoch stored in a tuple? e.g. List<Tuple<Long>> or something
-    //  - If we want to be thorough we should also probably be storing a starting and ending date for what
-    //    week this schedule starts and ends?
-    //  On second thought mysql supports a limited amount of datatypes - e.g. there is no list type, but there
-    //  are TIMESTAMP or DATETIME types (former taking locality into account), so I think we'll need this
-    //  column in tutor_listing to be a foreign key to a new table of timestamp/datetimes within an (arbitrary?) week
-    private Object availableTime;
+    @Column(name = "price_per_hour")
+    private double pricePerHour;
 
-    // TODO: datatype? a url to a pdf or something?
-    private Object resume;
+    @Column(name = "available_time")
+    private String availableTime;
 
+    @Column(name = "resume_path")
+    private String resumePath;
+
+    @Column(name = "description")
     private String description;
 
-    // TODO: what datatype is this? something like a String url to a video within the db?
-    //  How are videos retrieved and displayed from db
-    private Object tutoringVideoSample;
+    @Column(name = "video_sample_path")
+    private String tutoringVideoSamplePath;
 
-    // TODO: not sure what this is or what datatype it is
-    private Object live;
+    // Boolean but stored in mysql as an int: 0 or 1
+    @Column(name = "live")
+    private int live;
+
+    public Long getListingId()
+    {
+        return listingId;
+    }
+
+    public void setListingId(Long listingId)
+    {
+        this.listingId = listingId;
+    }
+
+    public Long getUserId()
+    {
+        return userId;
+    }
+
+    public void setUserId(Long userId)
+    {
+        this.userId = userId;
+    }
+
+    public Subject getSubject()
+    {
+        return subject;
+    }
+
+    public void setSubject(Subject subject)
+    {
+        this.subject = subject;
+    }
+
+    public Course getCourse()
+    {
+        return course;
+    }
+
+    public void setCourse(Course course)
+    {
+        this.course = course;
+    }
+
+    public double getPricePerHour()
+    {
+        return pricePerHour;
+    }
+
+    public void setPricePerHour(double pricePerHour)
+    {
+        this.pricePerHour = pricePerHour;
+    }
+
+    public String getAvailableTime()
+    {
+        return availableTime;
+    }
+
+    public void setAvailableTime(String availableTime)
+    {
+        this.availableTime = availableTime;
+    }
+
+    public String getResumePath()
+    {
+        return resumePath;
+    }
+
+    public void setResumePath(String resumePath)
+    {
+        this.resumePath = resumePath;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public String getTutoringVideoSamplePath()
+    {
+        return tutoringVideoSamplePath;
+    }
+
+    public void setTutoringVideoSamplePath(String tutoringVideoSamplePath)
+    {
+        this.tutoringVideoSamplePath = tutoringVideoSamplePath;
+    }
+
+    public int getLive()
+    {
+        return live;
+    }
+
+    public void setLive(int live)
+    {
+        this.live = live;
+    }
 
     @Override
     public String toString()
     {
-        return "TutorListing{" + "listingId=" + listingId + ", userId=" + userId + ", subjectId=" + subjectId + ", courseId=" + courseId + ", price=" + price + ", availableTime=" + availableTime + ", resume=" + resume + ", description='" + description + '\'' + ", tutoringVideoSample=" + tutoringVideoSample + ", live=" + live + '}';
+        return "TutorListing{" + "listingId=" + listingId + ", userId=" + userId + ", subject=" + subject + ", course=" + course + ", pricePerHour=" + pricePerHour + ", availableTime='" + availableTime + '\'' + ", resumePath='" + resumePath + '\'' + ", description='" + description + '\'' + ", tutoringVideoSamplePath='" + tutoringVideoSamplePath + '\'' + ", live=" + live + '}';
     }
 }
