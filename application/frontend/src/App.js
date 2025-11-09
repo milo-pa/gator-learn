2809ab3a33ba7ba7245d1ce662b004bbaa1eab47
RainyToaster <en.liganor@gmail.com>
2025-11-08 23:47:21 -0800

    Created new Home page

--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -23,17 +23,38 @@ class App extends Component {
 
                 <main>
                     <Routes>
-                        <Route path ="/" element={<div>HomePage</div>} />
-                        <Route path ="/results" element={<SearchResultsPage />} />
+                        <Route
+                            path="/"
+                            element={
+                                <div className="home-container">
+                                    <img src="/images/HomePage/CoolGator.webp" alt="Gator mascot studying" className="home-image"
+                                    />
+                                    <div className="home-text">
+                                        <h1>
+                                            Studying for Gators <br/> by Gators!
+                                        </h1>
+                                        <p>
+                                            Peer tutoring made easy by SF State students, for SF State
+                                            students. Get the help you need from classmates who’ve
+                                            been there — here at Gator Learn.
+                                        </p>
+                                        <p>
+                                            Ready to learn? Use the search bar to start your search
+                                            for tutors that teach your subject!
+                                        </p>
+                                    </div>
+                                </div>
+                            }
+                        />
+                        <Route path="/results" element={<SearchResultsPage />} />
                         <Route path="/team-members" element={<AboutPageApp />} />
                         <Route path="/team-members/:name" element={<DetailWithParams />} />
                     </Routes>
                 </main>
-            
             </Router>
-            
         );
     }
 }
+
 
 export default App;
