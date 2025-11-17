ef2fc7c2a38e8027a9e65ed5f350f501447dff11
Milo <milopesce@gmail.com>
2025-11-17 09:06:08 -0800

    Added Initial Login Page + Routing

---
+++ application/frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,26 @@
+/*
+ * Institution: San Francisco State University
+ * Class: CSC 648 Project, Team 05
+ * Project: Gator Learn, Tutoring Website
+ * Author: Milo Pesce Ares
+ * Created: 11/17/25
+ * Description: This is the login page
+ *
+ * Copyright (c) 2025 San Francisco State University Team 05
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import React, { Component } from "react";
+
+class LoginPage extends Component {
+    render() {
+        return (
+            <div className="login-page">
+                <h1>Login Page</h1>
+            </div>
+        );
+    }
+}
+
+export default LoginPage
