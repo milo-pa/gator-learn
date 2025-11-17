4485cb3e5303dc69b7b8d78bdd7cf77d339ff91f
Milo <milopesce@gmail.com>
2025-11-17 10:28:54 -0800

    Filled page in

--- application/frontend/src/pages/LoginPage.jsx
+++ application/frontend/src/pages/LoginPage.jsx
@@ -4,7 +4,7 @@
  * Project: Gator Learn, Tutoring Website
  * Author: Milo Pesce Ares
  * Created: 11/17/25
- * Description: This is the login page
+ * Description: This is the login page. nuff said
  *
  * Copyright (c) 2025 San Francisco State University Team 05
  *
@@ -14,13 +14,62 @@
 import React, { Component } from "react";
 
 class LoginPage extends Component {
-    render() {
-        return (
-            <div className="login-page">
-                <h1>Login Page</h1>
+  render() {
+    return (
+      <div className="login-page">
+        <header className="login-header">
+          <h1>Gator Learn Login</h1>
+          <div className="underline"></div>
+        </header>
+
+        <main className="login-main">
+          <form className="login-form">
+            {/* School Email */}
+            <div className="form-row">
+              <label htmlFor="school-email">School Email:</label>
+              <div className="input-wrapper with-suffix">
+                <input id="school-email" type="email" />
+                <span className="email-suffix">@sfsu.edu</span>
+              </div>
             </div>
-        );
-    }
+            {/* Password */}
+            <div className="form-row">
+              <label htmlFor="password">Password:</label>
+              <div className="input-wrapper">
+                <input id="password" type="password" />
+              </div>
+            </div>
+
+            {/* Buttons */}
+            <div className="button-row">
+              <button type="button" className="btn btn-secondary">
+                CANCEL
+              </button>
+              <button type="submit" className="btn btn-primary">
+                LOGIN
+              </button>
+            </div>
+
+            {/* Forgot Password */}
+            <div className="helper-row">
+              <p className="helper-text">Forgot your password?</p>
+              <button type="button" className="btn btn-light">
+                Click here
+              </button>
+            </div>
+
+            {/* Sign Up */}
+            <div className="helper-row">
+              <p className="helper-text">Don’t have an account?</p>
+              <button type="button" className="btn btn-secondary">
+                SIGN UP
+              </button>
+            </div>
+          </form>
+        </main>
+      </div>
+    );
+  }
 }
 
-export default LoginPage
+export default LoginPage;
