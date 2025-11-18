47985332d6b978c6489df413d8ab5df5b5a9845d
j-hammond1 <jhammond.123x@gmail.com>
2025-11-18 14:02:13 -0800

    Add Tutor Listing Page

---
+++ application/frontend/src/pages/TutorListingPage.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import TutorListingService from "../service/tutorsListingService";
+import "../css/loading.scss";
+import "../css/tutor-listing.scss";
+
+function TutorListingPage(props) {
+    const [listing, setListing] = useState(null);
+    const navigate = useNavigate();
+
+    function navigateBack() {
+        navigate(-1);
+    }
+
+    useEffect(() => {
+        async function loadListing() {
+            const listing = await TutorListingService.getListingById(props.params.id);
+            setListing(listing);
+        }
+
+        loadListing();
+    }, [props.params.id]);
+
+    if (!listing) {
+        return (
+            <div className="loading-wheel-container">
+                <div className="loading-wheel"></div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="tutor-container">
+            <h1 className="tutor-heading">Tutor Listing</h1>
+
+            <div className="tutor-top-row">
+                <div className="tutor-image-box">
+                    <img src={listing.account.photoPath} alt="User Account Photo"></img>
+                </div>
+
+                <div className="tutor-info-col">
+                    <div className="tutor-title">
+                        <span className="tutor-underline">{listing.account.name}</span> is tutoring {" "}
+                        <span className="tutor-underline">
+                            {listing.course.courseNumber} {listing.course.courseName}
+                        </span>
+                    </div>
+
+                    <div className="tutor-rate-row">
+                        <span>Rate: {listing.pricePerHour}.0$/hr</span>
+                    </div>
+
+                    <div>
+                        <div className="tutor-availability-title">Availability:</div>
+                        <div className="tutor-availability-box">{listing.availableTime}</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="tutor-description-wrap">
+                <div className="tutor-description-title">Description:</div>
+                <div className="tutor-description-box">{listing.description}</div>
+            </div>
+
+            <div className="tutor-buttons-row">
+                <button className="tutor-small-button">View Resume</button>
+                <button className="tutor-small-button">View Sample Video</button>
+                <button className="tutor-small-button">Contact</button>
+            </div>
+
+            <div className="tutor-back-wrap">
+                <button className="tutor-back-button" onClick={navigateBack}>Back</button>
+            </div>
+        </div>
+    );
+}
+
+export default TutorListingPage;
