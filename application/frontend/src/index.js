454a50f2663663c0ded74854409b078dedb8b88c
j-hammond1 <jhammond.123x@gmail.com>
2025-11-18 10:42:35 -0800

    Move css and scss files into src/css package

--- application/frontend/src/index.js
+++ application/frontend/src/index.js
@@ -12,7 +12,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 import React from "react";
-import "./index.css";
+import "./styles/index.css";
 import App from "./App";
 import { createRoot } from "react-dom/client";
 import { initAnalytics } from "./analytics/googleAnalytics";
