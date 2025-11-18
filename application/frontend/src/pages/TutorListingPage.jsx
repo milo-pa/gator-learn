71425f725ec4cc4cc270a92649eca16dca740cc9
j-hammond1 <jhammond.123x@gmail.com>
2025-11-18 14:12:29 -0800

    Move css files

--- application/frontend/src/pages/TutorListingPage.jsx
+++ application/frontend/src/pages/TutorListingPage.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import TutorListingService from "../service/tutorsListingService";
-import "../css/loading.scss";
-import "../css/tutor-listing.scss";
+import "../styles/loading.scss";
+import "../styles/tutor-listing.scss";
 
 function TutorListingPage(props) {
     const [listing, setListing] = useState(null);
