45307f9d14aadf1708932a20197427e909647662
smunthe <samanthachombo45@gmail.com>
2025-11-25 00:54:04 -0800

    Dashboard button moved to the right

--- application/frontend/src/component/MenuComponent.jsx
+++ application/frontend/src/component/MenuComponent.jsx
@@ -22,10 +22,10 @@ class MenuComponent extends Component {
             <div className="yellow-sticky">
                 {/* yub - yellow utility bar*/}
                 <nav className="yub-left">
-                    <Link className = "yub-link" to="/dashboard">Dashboard</Link>
                     <Link className = "yub-link" to="/results">Browse Listings</Link>
                     <Link className = "yub-link" to="/create-tutor-listing">Create Listing</Link>
-                    <Link className ="yub-link" to="/team-members">About</Link>
+                    <Link className = "yub-link" to="/team-members">About</Link>
+                    <Link className = "yub-link right-link" to="/dashboard">Dashboard</Link>
                 </nav>
                 <div className = "demo-line">
                     <em>SFSU Software Engineering Project CSC 648-848, Fall 2025. For Demonstration Only</em>
