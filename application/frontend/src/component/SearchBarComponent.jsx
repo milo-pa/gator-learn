7f3593e1a1c9075c32be0b43510e7356e91f6ade
smunthe <samanthachombo45@gmail.com>
2025-11-25 00:00:12 -0800

    Searching by subject is implemented

--- application/frontend/src/component/SearchBarComponent.jsx
+++ application/frontend/src/component/SearchBarComponent.jsx
@@ -13,51 +13,50 @@
  */
 import { useNavigate } from "react-router-dom";
 import React, { useState } from "react";
+import { SUBJECT_OPTIONS } from "../mock/mockOptions";
 
 function SearchBarComponent() {
-
-    const [mode, setMode] = useState("subject");
+    
+    const [subject, setSubject] = useState("all");
     const [text, setText] = useState("");
     const navigate = useNavigate();
 
     const handleSearch = (e) => {
         e.preventDefault();
-        if (mode === "all" ) {
-            navigate(`/results?subject=${encodeURIComponent(text)}&course=${encodeURIComponent(text)}`);
-        } else {
-            navigate(`/results?${mode}=${encodeURIComponent(text)}`);
-        }
+        const subjectParam = encodeURIComponent(subject);
+        const textParam = encodeURIComponent(text.trim());
+
+        navigate(`/results?subject=${subjectParam}&course=${textParam}`);
     };
 
     const handleDropdownChange = (e) => {
         const selectedValue = e.target.value;
-        setMode(selectedValue);
+        setSubject(selectedValue);
         setText("");
     };
 
-    const suggestions =
-        mode === "course"
-            ? "eg. CSC 648"
-            : mode === "subject"
-                ? "eg. Computer Science"
-                : "eg. Computer Science, CSC 648, etc.";
+
 
     return (
         <form className="search-container" onSubmit={handleSearch}>
             <select
                 className="search-category"
-                value={mode}
+                value={subject}
                 onChange={handleDropdownChange}
             >
-                <option value="all">All</option>
-                <option value="subject">Subject</option>
-                <option value="course">Course</option>
+                <option value="all">All Subjects</option>
+                {SUBJECT_OPTIONS.map((subj) => (
+                    <option key={subj.id} value={subj.name}>
+                        {subj.name}
+                    </option>
+                ))}
+    
             </select>
 
             <input
                 type="text"
                 className="search-input"
-                placeholder={suggestions}
+                placeholder="e.g., CSC 220, 220, Data Structures"
                 value={text}
                 onChange={(e) => setText(e.target.value)}
                 maxLength={40}
