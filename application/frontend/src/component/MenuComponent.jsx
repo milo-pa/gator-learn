6945ffe777d5970a00bf1d48923d565cefce8532
smunthe <samanthachombo45@gmail.com>
2025-11-25 00:57:32 -0800

    Moved demo line to purple bar

--- application/frontend/src/component/MenuComponent.jsx
+++ application/frontend/src/component/MenuComponent.jsx
@@ -27,9 +27,7 @@ class MenuComponent extends Component {
                     <Link className = "yub-link" to="/team-members">About</Link>
                     <Link className = "yub-link right-link" to="/dashboard">Dashboard</Link>
                 </nav>
-                <div className = "demo-line">
-                    <em>SFSU Software Engineering Project CSC 648-848, Fall 2025. For Demonstration Only</em>
-                </div>
+                
             </div>
         );
     }
