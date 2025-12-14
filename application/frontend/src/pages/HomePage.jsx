b42e9de5fc288c1ee4c8d6a69966c6340425cbd2
Milo <milopesce@gmail.com>
2025-12-14 15:33:25 -0800

    Removed unused component import in RegistrationPage and HomePage

--- application/frontend/src/pages/HomePage.jsx
+++ application/frontend/src/pages/HomePage.jsx
@@ -13,7 +13,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, { Component } from "react";
+import React from "react";
 import "../styles/HomePage.css";
 
 export default function HomePage() {
