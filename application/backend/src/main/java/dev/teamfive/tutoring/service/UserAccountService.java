de38f5bdb91141a03f7718dccb23768fce5c20c5
j-hammond1 <jhammond.123x@gmail.com>
2025-12-15 13:21:24 -0800

    Use spring boot security to create cookie session accounts, encrypt passwords, login/logout, etc

---
+++ application/backend/src/main/java/dev/teamfive/tutoring/service/UserAccountService.java
@@ -0,0 +1,49 @@
+package dev.teamfive.tutoring.service;
+
+import dev.teamfive.tutoring.model.UserAccount;
+import dev.teamfive.tutoring.repository.UserAccountRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserAccountService
+{
+    private final UserAccountRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserAccountService(UserAccountRepository userRepository, PasswordEncoder passwordEncoder)
+    {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public UserAccountRepository getUserRepository()
+    {
+        return userRepository;
+    }
+
+    public UserAccount createAccount(UserAccount user)
+    {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public Optional<UserAccount> authenticate(String email, String rawPassword)
+    {
+        Optional<UserAccount> userOpt = userRepository.findByEmail(email);
+
+        if (userOpt.isPresent())
+        {
+            UserAccount user = userOpt.get();
+
+            if (passwordEncoder.matches(rawPassword, user.getPassword()))
+            {
+                return Optional.of(user);
+            }
+        }
+        return Optional.empty();
+    }
+}
+
