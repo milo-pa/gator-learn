4f0ce77f9b4b01a940e7f654a90458f8b4244622
Milo <milopesce@gmail.com>
2025-12-19 03:04:19 -0800

    Added header block to media.js

--- application/frontend/src/util/media.js
+++ application/frontend/src/util/media.js
@@ -1,3 +1,18 @@
+/*
+ * Institution: San Francisco State University
+ * Class: CSC 648 Project, Team 05
+ * Project: Gator Learn, Tutoring Website
+ * Author: Milo Pesce Ares
+ * Created: 12/19/2025
+ * Description:
+ *  This utility file is used for routing media to port-forwarded port 8081 during local deployment
+ *
+ * Copyright (c) 2025 San Francisco State University Team 05
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const MEDIA_BASE_URL = (process.env.REACT_APP_MEDIA_BASE_URL || "").replace(/\/$/, "");
 
 export function buildMediaUrl(path) {
