d06d1cc1baf170328ecc2f2a7d6438d8609ab0b2
smunthe <samanthachombo45@gmail.com>
2025-12-19 03:09:27 -0800

    Headers on util, live listings only on pages, about styling page, componets comments

--- application/frontend/src/util/media.js
+++ application/frontend/src/util/media.js
@@ -1,3 +1,16 @@
+/*
+ * Institution: San Francisco State University
+ * Class: CSC 648 Project, Team 05
+ * Project: Gator Learn, Tutoring Website
+ * Author: Hill
+ * Created: 12/18/2025
+ * Description: this is for sending media files to database from frontend
+ *
+ * Copyright (c) 2025 San Francisco State University Team 05
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 const MEDIA_BASE_URL = (process.env.REACT_APP_MEDIA_BASE_URL || "").replace(/\/$/, "");
 
 export function buildMediaUrl(path) {
